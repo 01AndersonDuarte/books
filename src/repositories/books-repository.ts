33dbbc1b95e6a1af1063ef9d4ac3d286408bf637
Anderson Duarte <andersonduarte997@gmail.com>
2023-07-05 20:42:32 -0300

    First commit

---
+++ src/repositories/books-repository.ts
@@ -0,0 +1,36 @@
+import { CreateBook } from "../protocols/book";
+import { CreateReview } from "../protocols/review";
+
+import prisma from "../database";
+
+export async function getBooks() {
+  const result = await prisma.book.findMany();
+  return result;
+}
+
+export async function getBook(id: number) {
+  const result = await prisma.book.findFirst({
+    where: { id }
+  });
+
+  return result;
+}
+
+export async function createBook(book: CreateBook) {
+  const result = await prisma.book.create({
+    data: book
+  });
+
+  return result;
+}
+
+export async function reviewBook(bookReview: CreateReview) {
+  const { bookId, grade, review } = bookReview;
+
+  const result = await prisma.book.update({
+    where: { id: bookId },
+    data: { grade, review, read: true }
+  });
+
+  return result;
+}
